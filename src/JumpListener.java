272c329f3afee1b82f133a10a193be5ed58d2a1b
irnsght <irnsght@gmail.com>
2020-10-15 14:11:47 +0200

    added normal jumpdrive behavior, like loosing the charge when jumping, visual effects and 4 second delay.

--- src/JumpListener.java
+++ src/JumpListener.java
@@ -5,8 +5,14 @@ import api.listener.Listener;
 import api.listener.events.entity.ShipJumpEngageEvent;
 import api.mod.StarLoader;
 import org.schema.common.util.linAlg.Vector3i;
+import org.schema.game.common.controller.SegmentController;
+import org.schema.game.common.controller.Ship;
+import org.schema.game.common.controller.elements.jumpdrive.JumpAddOn;
+import org.schema.game.common.data.ManagedSegmentController;
 import org.schema.game.common.data.world.Sector;
 import org.schema.game.server.controller.SectorSwitch;
+import org.schema.schine.common.language.Lng;
+import org.schema.schine.network.server.ServerMessage;
 
 import javax.vecmath.Vector3f;
 
@@ -38,20 +44,49 @@ public class JumpListener {
                 Vector3i posNow = event.getOriginalSectorPos();
                 DebugFile.log("jump detected at y:" + posNow.y);
                 Vector3i newPos;
+                String displayMessage;
                 if (posNow.y >= 150) {
                     //is in warpspace, get realspace pos
                     ModPlayground.broadcastMessage("in warp");
+                    displayMessage = " out of warp to position ";
                     DebugFile.log("warp");
                     newPos = GetRealSpacePos(posNow);
                 } else {
                     //is in realspace, get warppos
                     ModPlayground.broadcastMessage("in realspace");
+                    displayMessage = " into warp to position ";
                     DebugFile.log("realspace");
                     newPos = GetWarpSpacePos(posNow);
                 }
-                SectorSwitch toWarp = GameServer.getServerState().getController().queueSectorSwitch(event.getController(),newPos,SectorSwitch.TRANS_JUMP,false);
+                SegmentController ship = event.getController();
+                //get jumpaddon
+                JumpAddOn warpdrive;
+                if(ship instanceof ManagedSegmentController<?>) {
+                    warpdrive =((Ship)ship).getManagerContainer().getJumpAddOn();
+                } else {
+                    DebugFile.log("entity " + ship.getName() + "tried jumping but is no managed SC.");
+                    return;
+                }
+
+                //TODO check for interdiction -> not possible bc vanilla method is private
+                //jump
+
+                SectorSwitch toWarp = GameServer.getServerState().getController().queueSectorSwitch(ship,newPos,SectorSwitch.TRANS_JUMP,false,true,true);
+                if (toWarp != null) {
+                    toWarp.delay = System.currentTimeMillis() + 4000;
+                    toWarp.jumpSpawnPos = new Vector3f(event.getController().getWorldTransform().origin);
+                    toWarp.executionGraphicsEffect = (byte) 2;
+                    toWarp.keepJumpBasisWithJumpPos = true;
+                    ship.sendControllingPlayersServerMessage(Lng.astr("Jumping " + displayMessage + " " + newPos.toStringPure()), ServerMessage.MESSAGE_TYPE_INFO);
+                    //empty jumpmodule after jump
+                    warpdrive.removeCharge();
+                    warpdrive.setCharge(0);
+                    warpdrive.sendChargeUpdate();
+                } else {
+                    ship.sendControllingPlayersServerMessage(Lng.astr("Jump failed, warpdrive needs to cooldown."), ServerMessage.MESSAGE_TYPE_INFO);
+                    DebugFile.log("jumping into warp failed");
+                }
                 //TODO why are jumps sometimes not executed?
-                //TODO empty jumpmodule like a jump ususally would
             }
         });
     }
