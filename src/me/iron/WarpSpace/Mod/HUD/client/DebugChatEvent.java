1fb9c0ec515621455798b75f858af4036fe05e0c
irnsght <irnsght@gmail.com>
2021-02-20 17:11:14 +0100

    quick cleanup of debugchatevent for HUD positioning

--- src/me/iron/WarpSpace/Mod/HUD/client/DebugChatEvent.java
+++ src/me/iron/WarpSpace/Mod/HUD/client/DebugChatEvent.java
@@ -33,18 +33,6 @@ public class DebugChatEvent {
                    ModPlayground.broadcastMessage("is not on server");
                    return;
                }
-               if (e.getText().contains("movehud")) { //movehud 1,-1
-                   //reduce string to first int
-                   String s = e.getText();
-                  s = s.replace("movehud ",""); //remove movehud
-                  s = s.replace(" ",""); //remove space
-                  DebugFile.log("after removing, string is: " + s);
-                  String[] parts = s.split(",");
-                  int x = Integer.parseInt(parts[0]);
-                  int y = Integer.parseInt(parts[1]);
-                  DebugFile.log("x and y are:" + new Vector2d(x,y).toString());
-                  moveHUDElement(x,y);
-               }
                if (e.getText().contains("domove")) { //move to absolute pixelpos
                    ModPlayground.broadcastMessage("doing move for all HUD stuff");
                    Integer[] integers = parseText(e.getText(),"domove",",");
@@ -83,13 +71,5 @@ public class DebugChatEvent {
         }
         DebugFile.log("integer[] is: " + arr.toString());
         return arr;
-    }
-
-
-    private static void moveHUDElement(int x, int y) {
-        textElement.setPos(textElement.getPos().x + x,textElement.getPos().y + y,0);
-    //    ModPlayground.broadcastMessage("text el is now at: " + textElement.getPos().toString());
-    //    DebugFile.log("text el is now at: " + textElement.getPos().toString());
-
     }
 }
