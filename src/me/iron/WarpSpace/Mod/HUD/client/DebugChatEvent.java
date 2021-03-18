d409da9192549b27cbc13d5fbda71b4f0b472bca
irnsght <irnsght@gmail.com>
2021-03-18 16:58:48 +0100

    Revert "quick cleanup of debugchatevent for HUD positioning"
    This reverts commit 14ff94c6

--- src/me/iron/WarpSpace/Mod/HUD/client/DebugChatEvent.java
+++ src/me/iron/WarpSpace/Mod/HUD/client/DebugChatEvent.java
@@ -33,6 +33,18 @@ public class DebugChatEvent {
                    ModPlayground.broadcastMessage("is not on server");
                    return;
                }
+               if (e.getText().contains("movehud")) { //movehud 1,-1
+                   //reduce string to first int
+                   String s = e.getText();
+                  s = s.replace("movehud ",""); //remove movehud
+                  s = s.replace(" ",""); //remove space
+                  DebugFile.log("after removing, string is: " + s);
+                  String[] parts = s.split(",");
+                  int x = Integer.parseInt(parts[0]);
+                  int y = Integer.parseInt(parts[1]);
+                  DebugFile.log("x and y are:" + new Vector2d(x,y).toString());
+                  moveHUDElement(x,y);
+               }
                if (e.getText().contains("domove")) { //move to absolute pixelpos
                    ModPlayground.broadcastMessage("doing move for all HUD stuff");
                    Integer[] integers = parseText(e.getText(),"domove",",");
@@ -64,12 +76,20 @@ public class DebugChatEvent {
         s = s.replace(keyword,""); //remove keyword
         s = s.replace(" ",""); //remove space
         DebugFile.log("pasreText: after removing keyword " + keyword + ", string is: " + s);
-        String[] parts = s.split(separator);
+        String[] parts = s.split(",");
         Integer[] arr = new Integer[parts.length];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = Integer.parseInt(parts[i]);
         }
         DebugFile.log("integer[] is: " + arr.toString());
         return arr;
+    }
+
+
+    private static void moveHUDElement(int x, int y) {
+        textElement.setPos(textElement.getPos().x + x,textElement.getPos().y + y,0);
+    //    ModPlayground.broadcastMessage("text el is now at: " + textElement.getPos().toString());
+    //    DebugFile.log("text el is now at: " + textElement.getPos().toString());
+
     }
 }
