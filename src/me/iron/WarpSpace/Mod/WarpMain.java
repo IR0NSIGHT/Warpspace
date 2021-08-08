b842b838cf99a593b06e07c2f14e0df339a94a08
irnsght <irnsght@gmail.com>
2021-08-08 20:31:48 +0200

    fixed class replacement for stl 0500

--- src/me/iron/WarpSpace/Mod/WarpMain.java
+++ src/me/iron/WarpSpace/Mod/WarpMain.java
@@ -1,6 +1,5 @@
 package me.iron.WarpSpace.Mod;
 
-import api.DebugFile;
 import api.listener.events.controller.ClientInitializeEvent;
 import api.listener.events.controller.ServerInitializeEvent;
 import api.mod.StarMod;
@@ -12,15 +11,9 @@ import me.iron.WarpSpace.Mod.server.WarpCheckLoop;
 import me.iron.WarpSpace.Mod.server.WarpJumpListener;
 import me.iron.WarpSpace.Mod.taswin.WarpSpaceMap;
 import me.iron.WarpSpace.Mod.visuals.BackgroundEventListener;
-import org.apache.commons.io.IOUtils;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.security.ProtectionDomain;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
 
-/**
+/*
  * STARMADE MOD
  * CREATOR: Max1M
  * DATE: 14.10.2020
@@ -71,34 +64,5 @@ public class WarpMain extends StarMod {
         GUIeventhandler.addHUDDrawListener();
         HUD_core.HUDLoop();
     }
-
-    private String[] classReplacements = new String[] {"/HudIndicatorOverlay","/ClientGameData"};
-    @Override
-    public byte[] onClassTransform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] byteCode) {
-        for (String classReplacementName : classReplacements) {
-            if(className.endsWith(classReplacementName)){
-                byte[] bytes = null;
-                try {
-                    ZipInputStream file = new ZipInputStream(new FileInputStream(this.getSkeleton().getJarFile()));
-                    while (true){
-                        ZipEntry nextEntry = file.getNextEntry();
-                        if(nextEntry == null) break;
-                        if(nextEntry.getName().endsWith(classReplacementName + ".class")){
-                            bytes = IOUtils.toByteArray(file);
-                        }
-                    }
-                    file.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                if(bytes != null){
-                    DebugFile.log("overwrote class '" + classReplacementName + "'",instance);
-                    return bytes;
-                }
-            }
-        }
-        return super.onClassTransform(loader, className, classBeingRedefined, protectionDomain, byteCode);
-    }
-
 
 }
